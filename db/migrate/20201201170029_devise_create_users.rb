05ea30503481596868b64defc599fca930e6220c
Texier <julestexier@live.fr>
2020-12-01 18:09:33 +0100

    Add Profil fields in User Table + Simple form

--- db/migrate/20201201170029_devise_create_users.rb
+++ db/migrate/20201201170029_devise_create_users.rb
@@ -7,6 +7,11 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+
+      ## Profil Info
+      t.string :name
+      t.integer :token_state, default: 1
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
