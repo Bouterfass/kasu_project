516fff86a22f915e118c515835a34fa40c080186
Youcef <cefyou28@gmail.com>
2020-12-02 17:24:49 +0100

    delete a manga from library works and library controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :mangas
   devise_for :users
   root "static_pages#index", page: "home"
-  resources :users, only: [:show] do 
-    resources :avatars, only: [:create]
-  end
+  resources :users
+  resources :library_items
 end
