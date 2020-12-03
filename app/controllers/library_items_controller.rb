4c57f007dded3d5969007a87f7d8e3f601c81371
Texier <julestexier@live.fr>
2020-12-03 11:15:27 +0100

    libraryItems Back end

--- app/controllers/library_items_controller.rb
+++ app/controllers/library_items_controller.rb
@@ -5,23 +5,32 @@ class LibraryItemsController < ApplicationController
     #localhost:3000/mon_profil/edit (destroy et redirection index mangas pour ajouter celui qu'il veut)
     #localhost:3000/manga/16/show
 
+    def new
+        @library_item = LibraryItem.new
+    end
+    
+    
     def create
-
-        @item = LibraryItem.new
-        
+        puts "Hola!!"
+        puts params
+        @item = LibraryItem.create(user: current_user, manga_id:params[:manga_id])
+        redirect_to edit_user_path(current_user)
         #localhost:3000/manga/:id/show
-        @item.create(user: current_user, manga_id: params[:id])
-
+        #@item.create(user: current_user, manga_id: params[:id])
     end
 
-    def destroy
+    def destroy 
         @item = LibraryItem.find(params[:id])
+        #@manga = Manga.find(params[:manga_id])
+        #@item = LibraryItem.where(user: current_user, manga_id:@manga.id)
+
+        puts @item
+        puts "Hello"
 
         @item.destroy
 
         redirect_to '/users/' + current_user.id.to_s + '/edit'
     end   
        
-
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :mangas
   devise_for :users
   root "static_pages#index", page: "home"
   resources :users
+  
   resources :library_items
 
+  resources :mangas, except: [:destroy] do
+    resources :library_items
+  end
 
-  resources :mangas, except: [:destroy] 
 end
