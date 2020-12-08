b02005404f51286137f127bdebb2ed7841e67280
Texier <julestexier@live.fr>
2020-12-08 12:58:55 -0300

    add Search Bar

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,15 @@ class StaticPagesController < ApplicationController
     @manga = LibraryItem.last(4)
     @user = User.last(3)
   end
+
+  def search  
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].downcase  
+      @results = Manga.all.where("lower(title) LIKE :search", search: "%#{@parameter}%")
+    end   
+  
+  end  
+
 end
