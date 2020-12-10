3a15995b8452cae0fac47565219b9ce6242f0bdf
Texier <julestexier@live.fr>
2020-12-10 11:45:59 -0300

    NavbarConceptPage

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,142 +13,142 @@ require 'open-uri'
 require 'faker'
 
 
-LibraryItem.delete_all
-LibraryItem.reset_pk_sequence
+# LibraryItem.delete_all
+# LibraryItem.reset_pk_sequence
 
-JoinMangaCategory.delete_all
-JoinMangaCategory.reset_pk_sequence
+# JoinMangaCategory.delete_all
+# JoinMangaCategory.reset_pk_sequence
 
 User.delete_all
 User.reset_pk_sequence
 
-Category.delete_all
-Category.reset_pk_sequence
+# Category.delete_all
+# Category.reset_pk_sequence
 
-Manga.delete_all
-Manga.reset_pk_sequence
+# Manga.delete_all
+# Manga.reset_pk_sequence
 
 
-def manga_scraper
+# def manga_scraper
 
-    manga_array = []
-    ("A".."Z").each do |letter|
-        puts ""
-        puts "Scraping mangas starting by letter : " + letter
-        page = Nokogiri::HTML(URI.open("https://www.manga-news.com/index.php/series/" + letter))
+#     manga_array = []
+#     ("A".."Z").each do |letter|
+#         puts ""
+#         puts "Scraping mangas starting by letter : " + letter
+#         page = Nokogiri::HTML(URI.open("https://www.manga-news.com/index.php/series/" + letter))
 
-        arr_title = page.xpath('//*[@id="seriesList"]/tbody/tr/td[1]/div/a/@title').to_a
-        titles = []
+#         arr_title = page.xpath('//*[@id="seriesList"]/tbody/tr/td[1]/div/a/@title').to_a
+#         titles = []
 
-        arr_title.each do |t|
-            titles << t.to_s
-        end
+#         arr_title.each do |t|
+#             titles << t.to_s
+#         end
 
-        puts "titles = " + titles.length.to_s
+#         puts "titles = " + titles.length.to_s
 
-        arr_image = page.xpath('.//*[@id="seriesList"]/tbody/tr/td[1]/div/img/@src').to_a
+#         arr_image = page.xpath('.//*[@id="seriesList"]/tbody/tr/td[1]/div/img/@src').to_a
 
-        images = []
+#         images = []
 
-        arr_image.each do |i|
-            images << i.text
-        end
+#         arr_image.each do |i|
+#             images << i.text
+#         end
 
-        puts "images = " + images.length.to_s
+#         puts "images = " + images.length.to_s
 
-        arr_authors = page.xpath('//*[@id="seriesList"]/tbody/tr/td[2]').to_a
+#         arr_authors = page.xpath('//*[@id="seriesList"]/tbody/tr/td[2]').to_a
 
-        authors = []
-        arr_authors.each do |aut|
-            if aut.text.include? "-" 
-                authors << aut.text.split("-")[1].gsub("\n", "").gsub("-","").strip!
-            else
-                authors << aut.text.gsub("\n", "").strip!
-            end
-        end
+#         authors = []
+#         arr_authors.each do |aut|
+#             if aut.text.include? "-" 
+#                 authors << aut.text.split("-")[1].gsub("\n", "").gsub("-","").strip!
+#             else
+#                 authors << aut.text.gsub("\n", "").strip!
+#             end
+#         end
 
-        puts "authors = " + authors.length.to_s
+#         puts "authors = " + authors.length.to_s
 
-        categories = []
-        page.xpath('//*[@id="seriesList"]/tbody/tr/td[5]/text()').each do |c|
-            categories << c.to_s.strip!
-        end
+#         categories = []
+#         page.xpath('//*[@id="seriesList"]/tbody/tr/td[5]/text()').each do |c|
+#             categories << c.to_s.strip!
+#         end
 
-        puts "categories = " +categories.length.to_s
+#         puts "categories = " +categories.length.to_s
 
-        volumes = []
+#         volumes = []
 
-        page.xpath('//*[@id="seriesList"]/tbody/tr/td[4]/text()').each do |v|
-            volumes << v.text.to_i
-        end
+#         page.xpath('//*[@id="seriesList"]/tbody/tr/td[4]/text()').each do |v|
+#             volumes << v.text.to_i
+#         end
 
-        puts "volumes = " +volumes.length.to_s
+#         puts "volumes = " +volumes.length.to_s
 
-        links = page.xpath('//*[@id="seriesList"]/tbody/tr/td[1]/div/a[1]/@href').to_a
+#         links = page.xpath('//*[@id="seriesList"]/tbody/tr/td[1]/div/a[1]/@href').to_a
 
-        descriptions = []
+#         descriptions = []
 
-        links.each do |l|
-            page_manga = Nokogiri::HTML(URI.open(l))
-            descriptions << page_manga.xpath('//*[@id="summary"]/p/text()').text
-        end
+#         links.each do |l|
+#             page_manga = Nokogiri::HTML(URI.open(l))
+#             descriptions << page_manga.xpath('//*[@id="summary"]/p/text()').text
+#         end
 
-        puts "descriptions = " + descriptions.length.to_s
+#         puts "descriptions = " + descriptions.length.to_s
 
-        i = 0 
+#         i = 0 
 
-        while i < titles.length
-            hash = {
-                :title => titles[i],
-                :author => authors[i],
-                :category => categories[i],
-                :image_url => images[i],
-                :volume => volumes[i],
-                :description => descriptions[i]
-            }
-            manga_array.push(hash)
-            i = i + 1
-        end
-    end
-    return manga_array
-end
+#         while i < titles.length
+#             hash = {
+#                 :title => titles[i],
+#                 :author => authors[i],
+#                 :category => categories[i],
+#                 :image_url => images[i],
+#                 :volume => volumes[i],
+#                 :description => descriptions[i]
+#             }
+#             manga_array.push(hash)
+#             i = i + 1
+#         end
+#     end
+#     return manga_array
+# end
 
-all_mangas = manga_scraper
-all_categories = []
+# all_mangas = manga_scraper
+# all_categories = []
 
-all_mangas.each do |category|
-    category[:category].split("|").each do |single|
-        all_categories << single.strip!
-    end
-end
+# all_mangas.each do |category|
+#     category[:category].split("|").each do |single|
+#         all_categories << single.strip!
+#     end
+# end
 
-all_categories = all_categories.uniq
+# all_categories = all_categories.uniq
 
-all_categories.each do |c|
-    Category.create(name: c)
-end
+# all_categories.each do |c|
+#     Category.create(name: c)
+# end
 
-puts all_categories
+# puts all_categories
 
-all_categories.each
+# all_categories.each
 
-all_mangas.each do |m|
-    Manga.create(
-        title: m[:title],
-        author: m[:author],
-        description: m[:description],
-        volume: m[:volume],
-        image_url: m[:image_url]
-    )
-end
+# all_mangas.each do |m|
+#     Manga.create(
+#         title: m[:title],
+#         author: m[:author],
+#         description: m[:description],
+#         volume: m[:volume],
+#         image_url: m[:image_url]
+#     )
+# end
 
-i = 1
-all_mangas.each do |manga|
-    manga[:category].split("|").each do |cat|
-        JoinMangaCategory.create(manga: Manga.find(i), category: Category.find_by(name: cat.strip!))
-    end
-    i += 1
-end
+# i = 1
+# all_mangas.each do |manga|
+#     manga[:category].split("|").each do |cat|
+#         JoinMangaCategory.create(manga: Manga.find(i), category: Category.find_by(name: cat.strip!))
+#     end
+#     i += 1
+# end
 
 gaetan = User.create(email: "badgaga@test.com", name: "Gaetan", password: "azerty")
 stephen = User.create(email: "stephen@test.com", name: "Stephen", password: "azerty")
@@ -156,18 +156,18 @@ jules = User.create(email: "jules@test.com", name: "Jules", password: "azerty")
 dylan = User.create(email: "dylan@test.com", name: "Dylan", password: "azerty")
 youcef = User.create(email: "youcef@test.com", name: "Youcef", password: "azerty", is_admin: true)
 
-10.times do 
-    LibraryItem.create(user: gaetan, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
-    LibraryItem.create(user: stephen, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
-    LibraryItem.create(user: jules, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
-    LibraryItem.create(user: dylan, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
-    LibraryItem.create(user: youcef, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
-end
+# 10.times do 
+#     LibraryItem.create(user: gaetan, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
+#     LibraryItem.create(user: stephen, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
+#     LibraryItem.create(user: jules, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
+#     LibraryItem.create(user: dylan, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
+#     LibraryItem.create(user: youcef, manga: Manga.find(rand(1..458)), state_description: Faker::Lorem.paragraph(sentence_count: 3))
+# end
 
-5.times do 
-    WishlistItem.create(user: gaetan, manga: Manga.find(rand(1..458)))
-    WishlistItem.create(user: stephen, manga: Manga.find(rand(1..458)))
-    WishlistItem.create(user: jules, manga: Manga.find(rand(1..458)))
-    WishlistItem.create(user: dylan, manga: Manga.find(rand(1..458)))
-    WishlistItem.create(user: youcef, manga: Manga.find(rand(1..458)))
-end
+# 5.times do 
+#     WishlistItem.create(user: gaetan, manga: Manga.find(rand(1..458)))
+#     WishlistItem.create(user: stephen, manga: Manga.find(rand(1..458)))
+#     WishlistItem.create(user: jules, manga: Manga.find(rand(1..458)))
+#     WishlistItem.create(user: dylan, manga: Manga.find(rand(1..458)))
+#     WishlistItem.create(user: youcef, manga: Manga.find(rand(1..458)))
+# end
